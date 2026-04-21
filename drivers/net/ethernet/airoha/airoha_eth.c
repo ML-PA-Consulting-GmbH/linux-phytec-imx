f4c84c42457a4e51ddf28989501a308ffea6a4b9
Lorenzo Bianconi <lorenzo@kernel.org>
2026-04-21 08:35:11 +0200

    net: airoha: fix BQL imbalance in TX path
    Fix a possible BQL imbalance in airoha_dev_xmit(), where inflight
    packets are accounted only for the AIROHA_NUM_TX_RING netdev TX
    queues. The queue index is computed as:
    
        qid = skb_get_queue_mapping(skb) % ARRAY_SIZE(qdma->q_tx)
        txq = netdev_get_tx_queue(dev, qid);
    
    However, airoha_qdma_tx_napi_poll() accounts completions across all
    netdev TX queues (num_tx_queues), leading to inconsistent BQL
    accounting.
    
    Also reset all netdev TX queues in the ndo_stop callback.
    
    Fixes: 1d304174106c ("net: airoha: Implement BQL support")
    Fixes: c9f947769b77 ("net: airoha: Reset BQL stopping the netdevice")
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Link: https://patch.msgid.link/20260421-airoha-fix-bql-v1-1-f135afe4275b@kernel.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 2d9f5a118205da2683ffcec78b9347f1f01a820e)

--- drivers/net/ethernet/airoha/airoha_eth.c
+++ drivers/net/ethernet/airoha/airoha_eth.c
@@ -918,10 +918,9 @@ static int airoha_qdma_tx_napi_poll(struct napi_struct *napi, int budget)
 			q->tail = (q->tail + 1) % q->ndesc;
 
 		if (skb) {
-			u16 queue = skb_get_queue_mapping(skb);
 			struct netdev_queue *txq;
 
-			txq = netdev_get_tx_queue(skb->dev, queue);
+			txq = skb_get_tx_queue(skb->dev, skb);
 			netdev_tx_completed_queue(txq, 1, skb->len);
 			if (netif_tx_queue_stopped(txq) &&
 			    q->ndesc - q->queued >= q->free_thr)
@@ -1662,7 +1661,7 @@ static int airoha_dev_stop(struct net_device *dev)
 	if (err)
 		return err;
 
-	for (i = 0; i < ARRAY_SIZE(qdma->q_tx); i++)
+	for (i = 0; i < dev->num_tx_queues; i++)
 		netdev_tx_reset_subqueue(dev, i);
 
 	if (atomic_dec_and_test(&qdma->users)) {
@@ -1937,7 +1936,7 @@ static netdev_tx_t airoha_dev_xmit(struct sk_buff *skb,
 
 	spin_lock_bh(&q->lock);
 
-	txq = netdev_get_tx_queue(dev, qid);
+	txq = skb_get_tx_queue(dev, skb);
 	nr_frags = 1 + skb_shinfo(skb)->nr_frags;
 
 	if (airoha_dev_tx_queue_busy(q, nr_frags)) {
