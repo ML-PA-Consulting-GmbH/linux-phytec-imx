6f8ee13df525aa6851ec46a5b7b2c6f3d98e567f
Wissem Boussetta <wissem.boussetta@outlook.com>
2026-07-13 17:13:27 +0200

    cve: apply CVE-2026-53274 via patch 35a221178396

--- drivers/spi/spi-mpc52xx.c
+++ drivers/spi/spi-mpc52xx.c
@@ -523,6 +523,12 @@ static void mpc52xx_spi_remove(struct platform_device *op)
 	struct mpc52xx_spi *ms = spi_controller_get_devdata(host);
 	int i;
 
+<<<<<<< ours
+=======
+	spi_unregister_controller(host);
+
+	cancel_work_sync(&ms->work);
+>>>>>>> theirs
 	free_irq(ms->irq0, ms);
 	free_irq(ms->irq1, ms);
 
@@ -532,7 +538,6 @@ static void mpc52xx_spi_remove(struct platform_device *op)
 		gpiod_put(ms->gpio_cs[i]);
 
 	kfree(ms->gpio_cs);
-	spi_unregister_controller(host);
 	iounmap(ms->regs);
 	spi_controller_put(host);
 }
